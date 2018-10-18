7eacc4a191165e08f1bb6bb4f4c5eac3b2617ebf
bagaking <zfaustk@qq.com>
2018-10-19 01:07:51 +0800

    feat (util): random

---
+++ src/EzSvrEngine/Utils/Random.cs
@@ -0,0 +1,22 @@
+﻿namespace EzSvrEngine.Utils
+{
+    public static class Random
+    {
+        private static readonly Random getrandom = new Random();
+
+        public static int RandInt32()
+        {
+            lock (getrandom) // synchronize
+            {
+                return getrandom.Next(Int32.MinValue, Int32.MaxValue);
+            }
+        }
+
+        public static int RandUInt32(){
+            lock (getrandom) // synchronize
+            {
+                return getrandom.Next(UInt32.MinValue, UInt32.MaxValue);
+            }
+        }
+    }
+}
